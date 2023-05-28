1161c6a8ae6750a6fafae86728230c143a7af63f
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-28 11:33:41 +0530

    create fileuploadutil.java

--- src/main/java/com/imageupload/StudentCRUD/Controller/StudentController.java
+++ src/main/java/com/imageupload/StudentCRUD/Controller/StudentController.java
@@ -2,6 +2,7 @@ package com.imageupload.StudentCRUD.Controller;
 
 import com.imageupload.StudentCRUD.Entity.Student;
 import com.imageupload.StudentCRUD.Service.StudentServiceImpl;
+import com.imageupload.StudentCRUD.Util.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
